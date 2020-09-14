9480932b12c6fc231d5e410ef838861b88bb93df
rahulvishwakarm <48653985+rahulvishwakarm@users.noreply.github.com>
2020-09-14 12:47:17 +0530

    Create Prac1_Desc.py

---
+++ Prac1_Desc.py
@@ -0,0 +1,10 @@
+#practical 1
+
+Steps:
+1. Open your telegram app and create contact with  botfather
+1. Create a new telegram bot by initializing the chat with botfather and type /newbot and name it
+1. Install telepot using pip install telepot command in cmd
+1. Make our first API call getMe() which returns the  information of the our bot
+1. getUpdates() gives the update of the bot
+1. Run the code with `python -i ./PRAC_1.py`
+1. The code will return the msg you sent to the bot as you said "msg"%
